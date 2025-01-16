3a6b978e70d2813062a4c6c6a5ba5c067733066f
khushirajput036 <kumarsinghajay516@gmail.com>
2025-01-16 05:32:19 +0000

    while_and_do_while_loop

---
+++ 04_iterator/while_do_while_loop.js
@@ -0,0 +1,20 @@
+
+index=0
+while(index<=10){
+    console.log(index)
+    index=index+2
+}
+
+let myArray = ['flash','superman','batman']
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(myArray[arr])
+    arr++
+}
+
+let score = 1
+do{
+    console.log(score)
+    score++
+}while(score <= 10)
