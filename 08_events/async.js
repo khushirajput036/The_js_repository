1e91aff995a52e995497ef94df35a2539a2a2b33
khushi <kumarsinghajay516@gmail.com>
2025-01-24 18:42:04 +0000

    async

---
+++ 08_events/async.js
@@ -0,0 +1,17 @@
+// Async
+
+// --> javascript -> sychronous ->single threaded // Default
+
+
+// --> Execution context -> execute one line of code at a time
+// console.log - 1
+// console.log - 2
+// Each operations wait for the last one to complete executing // call stack // Memory heap 
+
+
+
+// blocking code ->  block the code of flow the program -> read file sync
+// non blocking code -> doesn't block execution -> read file async
+
+
+
